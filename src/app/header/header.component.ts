4ab1af52f96c19352efdaa87deac1834e523d504
Mike Huang <wellwind@mail2000.com.tw>
2021-10-30 11:30:10 +0800

    實作任務 05、06

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  keyword = 'test';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  buttonClick(event: MouseEvent) {
+    console.log(event);
+    this.doSearch();
+  }
+
+  keyEnterUp() {
+    console.log('enter');
+    this.doSearch();
+  }
+
+  doSearch() {
+    this.keyword += '!';
+    console.log('search');
   }
 
 }
