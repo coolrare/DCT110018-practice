c7bfdf11c7f1ea201b82011d2224bac828cd20e0
Mike Huang <wellwind@mail2000.com.tw>
2021-10-30 10:36:55 +0800

    實作任務 03、04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DCT110018-practice';
+  title = 'conduitxxxx';
+  sbutitle = 'A place to share your <u>knowledge</u>.';
 }
